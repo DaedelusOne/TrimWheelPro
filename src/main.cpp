2203c86ce040b08270106b3d7fd3cfe8cf3df284
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-10-30 19:16:48 +0000

    using a number of turns const to make it easier to set the sensitivity

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-//#include "LibPrintf.h"
 #include "ResponsiveAnalogRead.h"
 #include "Joystick.h"
 
@@ -7,10 +6,9 @@
 #define DELAY 50
 #define MAX_ANALOG 1023.0
 #define DEGREES_PER_TURN 360
-#define MIN_JOYSTICK_RANGE 0
-#define MAX_JOYSTICK_RANGE 1440
+#define MAX_TURNS 4
 
-//The difference value that cannot be exceeded. Used to detect the sensor has been fully rotated or not
+//The threshold value that cannot be exceeded. Used to detect the sensor has been fully rotated or not
 #define FULL_ROTATION_DIFF_THRESHOLD (DEGREES_PER_TURN / 100) * 60 // 60% of degrees per turn
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
@@ -40,11 +38,11 @@ int getAngleDifference(int oldDegrees, int newDegrees) {
 
 __attribute__((unused))
 void setup() {
-    //Serial.begin(115200);
     prevAngle = readAnalogue();
-    joystickValue = (MAX_JOYSTICK_RANGE - MIN_JOYSTICK_RANGE) / 2;
+    int maxRange = DEGREES_PER_TURN * MAX_TURNS;
+    joystickValue = (maxRange) / 2;
     Joystick.begin();
-    Joystick.setRxAxisRange(MIN_JOYSTICK_RANGE, MAX_JOYSTICK_RANGE);
+    Joystick.setRxAxisRange(0, maxRange);
     Joystick.setRxAxis(joystickValue);
 }
 
@@ -55,7 +53,6 @@ void loop() {
     if (difference == 0) {
         Joystick.setRxAxis(joystickValue);
     } else {
-        //printf("Before: prevangle = %d currangle = %d joyvalue = %d difference = %d\n", prevAngle, currentAngle, joystickValue, difference);
         joystickValue += difference;
         Joystick.setRxAxis(joystickValue);
         prevAngle = currentAngle;
